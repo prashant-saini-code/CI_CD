72e100b16336c5c24bab319ed5dabe5420e9d995
Prashant Saini <prashantsaini5061@gmail.com>
2022-08-11 17:44:20 +0000

    Nginx Setup

---
+++ nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#!/bin/bash
+
+sudo cp -rf app.conf /etc/nginx/sites-available/app
+chmod 710 /var/lib/jenkins/workspace/CI_CD
+
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo nginx -t
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+echo "Nginx has been started"
+
+sudo systemctl status nginx
